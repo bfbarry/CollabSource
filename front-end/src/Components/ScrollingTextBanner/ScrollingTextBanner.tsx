24847be88aeff02f971b2adf85436678ad204cd4
Kaylen Melamed <kaylenmelamed@gmail.com>
2024-05-27 20:36:37 -0700

    remerging frontend

---
+++ front-end/src/Components/ScrollingTextBanner/ScrollingTextBanner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ScrollingTextBanner.css';
+
+const ScrollingTextBanner: React.FC = () => {
+    return(
+        <div id="scroll-text">Graphic Design   -   Digital Marketing   -   Software Engineering   -   3D Printing   -   UX Design   -   Art   -</div>
+    );
+}
+
+export default ScrollingTextBanner;
