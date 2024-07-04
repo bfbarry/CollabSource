275ada0e4de1daa5c083ec003268949bcf8acd5e
bfbarry <barry.brian.f@gmail.com>
2024-07-04 11:42:44 -0700

    logging package, not integrated yet

---
+++ back-end/logging/log_test.go
@@ -0,0 +1,56 @@
+package logging
+
+import (
+	"testing"
+	"os"
+	"bufio"
+	"regexp"
+)
+
+func getLines(fpath string) []string {
+	var lines []string
+	f, err := os.Open(fpath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	s := bufio.NewScanner(f)
+	i := 0
+	for s.Scan() {
+		line := s.Text()
+		lines = append(lines, line)
+		i += 1
+		if i == 2 { // in fail case, max would be 2
+			break
+		}
+	}
+	if err := s.Err(); err != nil {
+		panic(err)
+	}
+	return lines
+}
+
+func TestLog(t *testing.T) {
+	msg := "something"
+	fpath := "test_log.log"
+	if _, err := os.Stat(fpath); err == nil {
+		os.Remove(fpath)
+	}
+	InitLogEngine(fpath, "file", WARN)
+	Logger.Log(WARN, msg)
+	Logger.Log(DEBUG, msg) //assert this one didn't go through
+
+	logLines := getLines(fpath)
+	if len(logLines) != 1 {
+		t.Error("Log file should have 1 message")
+	}
+	line1 := logLines[0]
+	// TODO find regex for "2006-01-02 15:04:05"
+	messageFormatRegex := `^\[([A-Z]+)\]\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\]: (.*)`
+	re := regexp.MustCompile(messageFormatRegex)
+
+	matches := re.FindStringSubmatch(line1)
+	if matches == nil {
+		t.Error("Line 1 of log does not match message pattern")
+	}
+}
---
+++ back-end/logging/logging.go
@@ -0,0 +1,111 @@
+package logging
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+// useage...
+// logger = logging.NewLogEngine("logs.log", WARNING)
+// logger.log(INFO, "action occurred")
+type LogLevel int
+const (
+	DEBUG LogLevel = iota
+	INFO
+	WARN
+	ERROR
+	CRITICAL
+	MAX_LOG_LEVEL
+)
+type LogEngine struct {
+	uri string // e.g., file address
+	protocol string // e.g., local or server TODO make an enum
+	filterLevel LogLevel
+}
+
+var Logger *LogEngine
+
+type Msg struct {
+	level LogLevel
+	dateTime string
+	message string
+}
+
+var messageFormat string
+// UTILS
+
+func init() {
+	messageFormat = "[%s][%s]: %s\n" //level, dateTime, message
+}
+
+func levelAsString(l LogLevel) string {
+	switch l {
+	case DEBUG:
+		return "DEBUG" 
+	case INFO:
+		return "INFO" 
+	case WARN:
+		return "WARN" 
+	case ERROR:
+		return "ERROR" 
+	case CRITICAL:
+		return "CRITICAL"
+	default:
+		return fmt.Sprintf("INVALID_LOG_LEVEL_%d", l)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func getCurrentTime() string {
+	formatStr := "2006-01-02 15:04:05" //don't change
+	return time.Now().Format(formatStr)
+}
+
+func InitLogEngine(uri string, protocol string, filterLevel LogLevel) {
+	Logger = &LogEngine{
+		uri: uri,
+		protocol: protocol,
+		filterLevel: filterLevel,
+	}
+	switch protocol {//TODO
+	case "file":
+		;
+	case "stdout":
+		;
+	}
+}
+
+func newLog(level LogLevel, message string) *Msg {
+	return &Msg{
+		level: level,
+		dateTime: getCurrentTime(),
+		message: message,
+	}
+}
+
+func (this *LogEngine) Log(level LogLevel, message string) {
+	// TODO log levels
+	l := newLog(level, message)
+	if level < this.filterLevel {
+		return
+	}
+	fmtLog := fmt.Sprintf(messageFormat, levelAsString(l.level), l.dateTime, l.message)
+	switch this.protocol {
+	case "stdout":
+		log.Println(fmtLog)
+	case "file":
+		f, err := os.OpenFile(this.uri, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		checkErr(err)
+		defer f.Close()
+
+		_, err = f.WriteString(fmtLog)
+		checkErr(err)
+
+	}
+
+}
